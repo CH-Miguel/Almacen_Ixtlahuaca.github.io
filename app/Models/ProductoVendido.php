11dd1896378967c83329143c43c981933526c1ea
CH-Miguel <73492130+CH-Miguel@users.noreply.github.com>
2022-07-06 20:35:25 -0500

    Add files via upload

---
+++ app/Models/ProductoVendido.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductoVendido extends Model
+{
+    use HasFactory;
+    protected $table = "productos_vendidos";
+    protected $fillable = ["id_venta", "descripcion", "codigo_barras", "precio", "cantidad"];
+
+}
---
+++ app/Models/Venta.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Venta extends Model
+{
+    use HasFactory;
+    public function productos()
+    {
+        return $this->hasMany("App\Models\ProductoVendido", "id_venta");
+    }
+
+    public function cliente()
+    {
+        return $this->belongsTo("App\Models\Cliente", "id_cliente");
+    }
+}
